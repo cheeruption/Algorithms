6ef7c18f0a10e78a45787fe6b7352ca1947d2df1
cheeruption <cheeruption@gmail.com>
2018-08-27 14:53:31 +0300

    Revert "ДЗ (9 заданий) к Уроку 1."
    This reverts commit f753d5459faf17d0d5bb2a464f580b91c58efd4f.

--- L1/t5.py
+++
@@ -1,9 +0,0 @@
-a = input('Введите две латинские буквы через запятую:')
-
-from re import findall
-
-pos1 =ord(findall('^\w',a)[0])-96
-pos2 =ord(findall('\w$',a)[0])-96
-dist = abs(pos2-pos1-1)
-
-print('Первая буква номер',pos1,'Вторая буква номер',pos2,'Между ними',dist,'символ(-ов)')
--- L1/t6.py
+++
@@ -1,3 +0,0 @@
-bukva = int(input('Введите номер буквы от 1 до 26: '))
-bukva = chr(96+bukva)
-print(bukva)
